a40bcb791886d658a2e3474f09d70f60f6e10f32
zayyadi <32278136+zayyadi@users.noreply.github.com>
2021-04-17 16:31:53 +0100

    Add files via upload

--- jsonf.py
+++ jsonf.py
@@ -1,8 +1,12 @@
 import requests
 import json
 res = requests.get('http://httpbin.org/get')
-print(res.status_code)  
-print(res.text)    
+#print(res.status_code)  
+#print(res.text)    
+resj = res.json()
 
-for i in res.json():
-    print (i)
+json_str = json.dumps(resj)
+resp = json.loads(json_str)
+#print (resp)
+for values in resp:
+    print (resp[values])
